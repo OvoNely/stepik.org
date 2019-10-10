fbf2001291931f0d9a2eecb3d707638ab0508803
Grigory <48507300+n-gregory-a@users.noreply.github.com>
2019-10-10 14:29:22 +0700

    Create TheLongestWord.java

---
+++ Course: Adaptive Java/TheLongestWord.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TheLongestWord {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int count = 0;
+		int maxCount = 0;
+		String result = "";
+		
+		while(scanner.hasNext()) {
+			String str = scanner.next();
+			count = str.length();
+			if (count > maxCount) {
+				result = str;
+				maxCount = str.length();
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+
+}
